895769807f3a014374c1356958b8b5dd8b06fea3
Abhishek Gorya <abhishekgorya8@gmail.com>
2026-08-25 11:02:51 +0530

    Time: 1105 ms (54.53%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,16 +1,14 @@
 
-SELECT
-    d.name AS Department,
-    e.name AS Employee,
-    e.salary AS Salary
-FROM
-    Employee e
-    JOIN Department d ON e.departmentId = d.id
-WHERE
-    (
-        SELECT COUNT(DISTINCT salary)
-        FROM Employee e2
-        WHERE e2.departmentId = e.departmentId AND e2.salary >= e.salary
-    ) <= 3
-ORDER BY
-    Department, Salary DESC;
+WITH new_table AS (
+    SELECT 
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+        DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS ranking
+    FROM Employee e
+    LEFT JOIN Department d
+    ON e.departmentId = d.id
+)
+SELECT Department, Employee, Salary
+FROM new_table
+WHERE ranking <= 3;
